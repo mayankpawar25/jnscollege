fd3ebc4aa0ffebf3064eedfe02ee9ae03002c29a
Mayank <mayank.pawar@webcraftit.com>
2024-01-05 15:05:46 +0530

    first init

---
+++ application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = 'Nai Pata';
+$config['envato_market_username']      = 'Sahihaiboss';
+$config['SSLK'] = 'di8xQ0-Zrd0lP-NGUzcU-NPVVBBZDdFQmo1S2lHQitjTWdVN211TVJOc3hMND0=';
+$config['app_ver'] = 1;
